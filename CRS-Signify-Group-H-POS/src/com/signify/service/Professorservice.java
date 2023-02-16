14caabf22d2dd15bb40beab81c0ab93ea3cb7e8a
Palakj62 <91754137+Palakj62@users.noreply.github.com>
2023-02-16 09:42:43 +0530

    Add files via upload

---
+++ CRS-Signify-Group-H-POS/src/com/signify/service/Professorservice.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.signify.service;
+
+/**
+ * @author palak
+ *
+ */
+public class Professorservice {
+	public void viewEnrolledStudents()
+	{
+		System.out.println("Enrolled students name");
+	}
+	
+	public void addGrade()
+	{
+		System.out.println("Add grade");
+	}
+}
